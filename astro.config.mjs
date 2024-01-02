fb9f095d31eb02a21ad826c83fceec2616269eab
kombumori <dev.kombumori@gmail.com>
2024-01-02 15:54:14 +0900

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()]
+  site: 'https://noto-support.vercel.app',
+  integrations: [tailwind(), preact(), sitemap()]
 });
