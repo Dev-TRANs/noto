e95d7caab03c16dfb82affdc81c6551bfe309f1e
kombumori <76637001+kombumori@users.noreply.github.com>
2024-01-30 18:37:27 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://noto-support.vercel.app',
+  site: 'https://noto.stki.org',
   integrations: [tailwind(), preact(), sitemap()]
 });
